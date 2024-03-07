e1003e65da91b700559e91b27593a2e4662195c4
Maciej Kusz <9740637+mkusz@users.noreply.github.com>
2024-03-07 10:56:34 +0100

    playwright page

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,3 +33,22 @@ def playwright_browser(
             yield browser
         finally:
             browser.close()
+
+
+@pytest.fixture()
+def playwright_page(
+    playwright_browser: playwright.Browser,
+    playwright_config: configs.PlaywrightConfig,
+    env_config: configs.EnvConfig,
+) -> playwright.Page:
+    browser_context = playwright_browser.new_context()
+
+    new_page: playwright.Page = browser_context.new_page()
+
+    with new_page as page:
+        page.set_default_timeout(playwright_config.elements_timeout_sec * 1000)
+        page.set_default_navigation_timeout(playwright_config.navigation_timeout_sec * 1000)
+
+        yield page
+
+    browser_context.close()
