1c35c82a5dca2697f86a330f931154ea04d1131f
Maciej Kusz <9740637+mkusz@users.noreply.github.com>
2024-03-07 10:50:14 +0100

    playwright browser

--- src/configs.py
+++ src/configs.py
@@ -10,3 +10,11 @@ class PlaywrightConfig(pydantic_settings.BaseSettings):
 
     browser: str = ""
     headless: bool = True
+
+
+class EnvConfig(pydantic_settings.BaseSettings):
+    model_config = pydantic_settings.SettingsConfigDict(
+        env_prefix="QA_ENV_", env_file=".env", frozen=True
+    )
+
+    artifacts_dir: str = "artifacts/"
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,35 @@
 import pytest
 from src import configs
+import playwright.sync_api as playwright
 
 
 @pytest.fixture(scope="session")
 def playwright_config() -> configs.PlaywrightConfig:
     return configs.PlaywrightConfig()
+
+
+@pytest.fixture(scope="session")
+def env_config() -> configs.EnvConfig:
+    return configs.EnvConfig()
+
+
+@pytest.fixture()
+def playwright_browser(
+    playwright_config: configs.PlaywrightConfig, env_config: configs.PlaywrightConfig
+) -> playwright.Browser:
+    with playwright.sync_playwright() as playwright_obj:
+        browser: playwright.Browser
+        if playwright_config.browser == "chromium":
+            browser = playwright_obj.chromium.launch(
+                channel=playwright_config.browser,
+                headless=bool(playwright_config.headless),
+                args=["--disable-gpu"],
+                traces_dir=env_config.artifacts_dir,
+            )
+        else:
+            raise SystemError(f"'{playwright_config.browser}' is not supported (yet)")
+
+        try:
+            yield browser
+        finally:
+            browser.close()
